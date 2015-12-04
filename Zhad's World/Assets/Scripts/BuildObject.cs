3b0ccab2a266ef413962c5ac8bf91cd658f1383e
ChuuRufflez <jimfeenema@gmail.com>
2015-12-04 21:31:42 +0100

    Commit1

---
+++ Zhad's World/Assets/Scripts/BuildObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildObject : MonoBehaviour {
+	
+	public GameObject gatePrefab;
+	public Transform player;
+	public Vector3 spawnRotation = new Vector3(90,0,0);
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			Instantiate(gatePrefab, player.position, player.rotation);
+		}
+	}
+}
