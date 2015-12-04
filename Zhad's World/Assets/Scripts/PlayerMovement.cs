549b7d5f66ebd450af10baa5aa69547dc14c1c8c
ChuuRufflez <jimfeenema@gmail.com>
2015-12-04 20:32:32 +0100

    Initial Commit

---
+++ Zhad's World/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float movementSpeed = 10.0f;
+	public float rotateSpeed = 50.0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)) {
+			MoveForward();
+		}
+		
+		if (Input.GetKey(KeyCode.D)) {
+			TurnRightward();
+		}
+		
+		if (Input.GetKey(KeyCode.A)) {
+			TurnLeftward();
+		}
+		
+		if (Input.GetKey(KeyCode.S)) {
+			MoveBackward();
+		}
+	}
+	
+	void MoveForward () {
+		transform.Translate((Vector3.forward) * movementSpeed * Time.deltaTime);
+	}
+	
+	void TurnRightward () {
+		transform.Rotate((Vector3.up) * rotateSpeed * Time.deltaTime);
+	}
+	
+	void TurnLeftward () {
+		transform.Rotate((Vector3.up) * -rotateSpeed * Time.deltaTime);
+	}
+	
+	void MoveBackward () {
+		transform.Translate((Vector3.back) * movementSpeed * Time.deltaTime);
+	}
+}
---
+++ Zhad's World/Assets/Scripts/WalkAnimationZhad.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkAnimationZhad : MonoBehaviour {
+
+	public AnimationClip walkAnimation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)) {
+			GetComponent<Animation>().Play ();
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			GetComponent<Animation>().Play ();
+		}
+		
+//		if (Input.GetKeyUp(KeyCode.W)) {
+//			GetComponent<Animation>().Stop ();
+//		}
+//		if (Input.GetKeyUp(KeyCode.S)) {
+//			GetComponent<Animation>().Stop ();
+//		}
+	}
+}
